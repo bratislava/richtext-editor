5fa2206c90bd7035dbefa6ea479eb0c2acdbd3cd
PavelFilo <pvlfilo@gmail.com>
2024-11-15 17:48:53 +0100

    Fix tooltip visibility on fullscreen

--- packages/react/src/entry/FullscreenWrap.tsx
+++ packages/react/src/entry/FullscreenWrap.tsx
@@ -12,7 +12,7 @@ const $Container = styled("div")`
     left: 0;
     right: 0;
     bottom: 0;
-    z-index: 20000;
+    z-index: 999;
   }
 `
 
--- packages/react/src/use-tooltip/triangle.tsx
+++ packages/react/src/use-tooltip/triangle.tsx
@@ -7,9 +7,9 @@ const $Triangle = styled("span")`
   z-index: 10;
   width: 0;
   height: 0;
-  border-left: 0.375em solid transparent;
-  border-right: 0.375em solid transparent;
-  border-top: 0.375em solid var(--shade-700);
+  border-top: 0.375em solid transparent;
+  border-bottom: 0.375em solid transparent;
+  border-right: 0.375em solid var(--shade-700);
 `
 
 export function Triangle({ dest }: { dest: HTMLElement }) {
@@ -17,8 +17,12 @@ export function Triangle({ dest }: { dest: HTMLElement }) {
   return (
     <$Triangle
       style={{
-        left: `calc(${rect.left + rect.width / 2}px - 0.375em)`,
-        top: `calc(${rect.top}px - 0.5em)`,
+        // Right triangle
+        left: rect.right,
+        top: `calc(${rect.top + rect.height / 2}px - 0.375em)`,
+        // Top triangle
+        // left: `calc(${rect.left + rect.width / 2}px - 0.375em)`,
+        // top: `calc(${rect.top}px - 0.5em)`,
       }}
     />
   )
